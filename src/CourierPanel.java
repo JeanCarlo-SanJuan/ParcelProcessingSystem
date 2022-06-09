d4e650335345c8e4c7df0dc3789da08e4f22d5e1
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-09 18:04:08 +0800

    made the preview elements public and cleanup

--- src/CourierPanel.java
+++ src/CourierPanel.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CourierPanel extends Panel {
+    //We need the frame(parent) to be able to directly change the text
+    public JLabel previewID = new JLabel("");
+    public JLabel previewStatus = new JLabel("");
+    public JLabel previewMode = new JLabel("");
+    
     public CourierPanel(Courier courier) {
         BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         this.setLayout(boxlayout);
@@ -9,46 +14,36 @@ public class CourierPanel extends Panel {
         JLabel title = new JLabel("Preview");
         title.setFont(new Font("Tahoma", Font.BOLD,20));
         title.setOpaque(true);
-        title.setHorizontalAlignment(SwingConstants.CENTER);
         this.add(title);
 
         JLabel col1 = new JLabel("Parcel ID");
         col1.setFont(new Font("Tahoma", Font.BOLD,15));
         col1.setBackground(Color.LIGHT_GRAY);
         col1.setOpaque(true);
-        col1.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(col1);
+        this.add(col1); 
         
-        JLabel info1 = new JLabel(courier.parcel.parcelId);
-        info1.setOpaque(true);
-        info1.setHorizontalAlignment(SwingConstants.CENTER);
-        info1.setVerticalAlignment(SwingConstants.NORTH);
-        this.add(info1);
+        previewID.setText(courier.parcel.parcelId);
+        previewID.setOpaque(true);
+        this.add(previewID);
 
         JLabel col2 = new JLabel("Status");
         col2.setFont(new Font("Tahomar", Font.BOLD,15));
         col2.setBackground(Color.LIGHT_GRAY);
         col2.setOpaque(true);
-        col2.setHorizontalAlignment(SwingConstants.CENTER);
         this.add(col2);
         
-        JLabel info2 = new JLabel("" + courier.checkProgress());
-        info2.setOpaque(true);
-        info2.setHorizontalAlignment(SwingConstants.CENTER);
-        info2.setVerticalAlignment(SwingConstants.NORTH);
-        this.add(info2);
+        previewStatus.setText("" + courier.checkProgress());
+        previewStatus.setOpaque(true);
+        this.add(previewStatus);
 
         JLabel col3 = new JLabel("Mode");
         col3.setBackground(Color.LIGHT_GRAY);
         col3.setFont(new Font("Tahoma", Font.BOLD,15));
         col3.setOpaque(true);
-        col3.setHorizontalAlignment(SwingConstants.CENTER);
         this.add(col3);
 
-        JLabel info3 = new JLabel("" + courier.delivery.getMode());
-        info3.setOpaque(true);
-        info3.setHorizontalAlignment(SwingConstants.CENTER);
-        info3.setVerticalAlignment(SwingConstants.NORTH);
-        this.add(info3);
+        previewMode.setText("" + courier.delivery.getMode());
+        previewMode.setOpaque(true);
+        this.add(previewMode);
     }
 }
