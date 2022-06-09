bacfdc23ca8046d120e55a68c27bc3fcd507ff47
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2022-06-09 16:15:00 +0800

    Refactor MainFrame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,43 +6,68 @@ import javax.swing.JLabel;
 import java.awt.Dimension;
 import java.awt.Font;
 
-import java.awt.event.WindowAdapter;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class MainFrame extends JFrame {
-    private JPanel mainPane;
-    private ParcelController PC;
-    private ButtonGroup options;
+    private static final int GAPS = 10;
+    private ParcelController parcelController;
+    private JPanel buttonsPanel;
+    private ButtonGroup buttonsGroup;
 
-    public MainFrame(ParcelController pc) {
-        this.PC = pc;
-        setTitle("Main Window");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 1280, 720);
-        mainPane = new JPanel();
-        mainPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(mainPane);
-        mainPane.setLayout(null);
+    public MainFrame(ParcelController parcelController) {
+        this.parcelController = parcelController;
+        this.initializeComponents();
+    }
 
-        JLabel lblProcessed = new JLabel("Processed Parcels: Sorted by ID");
-        lblProcessed.setFont(new Font("Tahoma", Font.BOLD, 30));
-        lblProcessed.setBounds(40, 60, 700, 80);
-        mainPane.add(lblProcessed);
+    private void initializeComponents() {
+        setTitle("Parcel Input Form");
+        setPreferredSize(new Dimension(1280, 720));
 
-        JButton btn_add_parcel = new JButton("NEW PARCEL");
-        btn_add_parcel.setFont(new Font("Tahoma", Font.BOLD, 20));
-        btn_add_parcel.setBounds(1000, 120, 200, 80);
-        btn_add_parcel.addActionListener(new ActionListener() {
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.LINE_AXIS));
+        getRootPane().setBorder(new EmptyBorder(GAPS, GAPS, GAPS, GAPS));
 
-            @Override
+        createLeft();
+        createRight();
+
+        pack();
+    }
+
+    private void createLeft() {
+        var leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        leftPanel.setBorder(new EmptyBorder(GAPS, GAPS, GAPS, GAPS));
+
+        var titleLabel = new JLabel("Processed Parcels: Sorted by ID");
+        titleLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        leftPanel.add(titleLabel);
+
+        leftPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+        buttonsPanel = new JPanel();
+        var buttonsPanelScroll = new JScrollPane(buttonsPanel);
+        buttonsPanelScroll.setBorder(BorderFactory.createEmptyBorder());
+        buttonsGroup = new ButtonGroup();
+        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.PAGE_AXIS));
+        addParcels();
+        leftPanel.add(buttonsPanelScroll);
+
+        add(leftPanel, BorderLayout.LINE_START);
+    }
+
+    private void createRight() {
+        var rightPanel = new JPanel();
+        rightPanel.setLayout(new GridLayout(4, 0, GAPS * 2, GAPS * 2));
+        rightPanel.setBorder(new EmptyBorder(GAPS, GAPS, GAPS, GAPS));
+        rightPanel.setMaximumSize(new Dimension(75, 32767));
+
+        var newParcelButton = new JButton("New Parcel");
+        newParcelButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        newParcelButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                ParcelFrame frame = new ParcelFrame(PC);
-                frame.setPreferredSize(new Dimension(720, 720));
-                frame.pack();
+                ParcelFrame frame = new ParcelFrame(parcelController);
                 frame.setVisible(true);
                 frame.addWindowListener( new WindowAdapter() {
                     @Override
@@ -53,48 +78,69 @@ public class MainFrame extends JFrame {
 
             }
         });
-        mainPane.add(btn_add_parcel);
+        rightPanel.add(newParcelButton);
 
-        JButton btn_logout = new JButton("LOG OUT");
-        btn_logout.setFont(new Font("Tahoma", Font.BOLD, 20));
-        btn_logout.setBounds(1000, 240, 200, 80);
-        btn_logout.addActionListener(new ActionListener() {
+        var statusButton = new JButton("Delivery Status");
+        statusButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        statusButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 
-            @Override
+            }
+        });
+        rightPanel.add(statusButton);
+
+        var infoButton = new JButton("Parcel Info");
+        infoButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        infoButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        rightPanel.add(infoButton);
+
+        var logoutButton = new JButton("Log Out");
+        logoutButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        logoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 MainFrame.this.dispose();
             }
         });
-        mainPane.add(btn_logout);
+        rightPanel.add(logoutButton);
 
-        JButton btn_delivery = new JButton("Delivery Status");
-        btn_delivery.setFont(new Font("Tahoma", Font.BOLD, 19));
-        btn_delivery.setBounds(1000, 360, 200, 80);
-        btn_delivery.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        mainPane.add(btn_delivery);
-
-        JButton btn_info = new JButton("Parcel Info");
-        btn_info.setFont(new Font("Tahoma", Font.BOLD, 20));
-        btn_info.setBounds(1000, 480, 200, 80);
-        btn_info.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        mainPane.add(btn_info);
-        updateList();
+        add(rightPanel, BorderLayout.LINE_END);
     }
 
-    public void updateList() {
-        ButtonGroup bg = new ButtonGroup();
-        ParcelButtonsGenerator.createButtonsFromList( this.PC.getParcels(), bg, mainPane, 76, 194, 50, 300, 23);
-        pack();
+    private void addParcels() {
+        var parcels = parcelController.getParcels();
+        for (int i = 0; i < parcels.size(); i++) {
+            var parcel = parcels.get(i);
+            var parcelButton = new JRadioButton((i + 1) + ". " + parcel.parcelId);
+            parcelButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+            parcelButton.setActionCommand(parcel.parcelId);
+            buttonsPanel.add(parcelButton);
+            buttonsGroup.add(parcelButton);
+        }
+    }
+    
+    private void refreshParcels() {
+        buttonsPanel.removeAll();
+        buttonsGroup = new ButtonGroup();
+        addParcels();
+        revalidate();
+        repaint();
+    }
+    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    try {
+                        var parcelController = new ParcelController();
+                        var mainFrame = new MainFrame(parcelController);
+                        mainFrame.setVisible(true);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
     }
 }
